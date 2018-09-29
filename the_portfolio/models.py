82b38db44af3144bc357f41d54af5bf77a278459
ABOD <abod80000@gmail.com>
2018-09-30 00:38:58 +0200

    Object Model Created

--- the_portfolio/models.py
+++ the_portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Object(models.Model):
+	object_name = models.CharField(max_length=130)
+
+
+
+	def __str__(self):
+		return self.object_name
--- the_portfolio/views.py
+++ the_portfolio/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .models import Object
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'the_portfolio/index.html')
+	objects = Object.objects.all()
+	context = {'objects' : objects}
+	return render(request, "the_portfolio/index.html" , context)
+def crazy(request):
+	if request.method == "POST":
+		object_name = request.POST.get('object_name')
+		Object.objects.create(object_name=object_name)
+		return redirect('index')
+
+	return render(request , 'the_portfolio/create_object.html')
