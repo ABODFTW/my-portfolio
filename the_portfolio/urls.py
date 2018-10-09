4d83a5ef528d306f76cd59d1a06e1a2e64641904
ABOD <abod80000@gmail.com>
2018-10-09 13:04:11 +0200

    Project Model Added.

--- the_portfolio/urls.py
+++ the_portfolio/urls.py
@@ -3,5 +3,6 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('' , views.index , name="index" ),
+    path('' , views.index.as_view() , name="index" ),
+    path('add-project' , views.AddProject.as_view() , name='add-project')
 ]
--- the_portfolio/views.py
+++ the_portfolio/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render , redirect
+from django.views import View
+from django.views.generic.base import *
+from django.views.generic.edit import * 
+from .models import * 
 # Create your views here.
 
+class index(TemplateView):
+	template_name = 'the_portfolio/index.html'
 
-def index(request):
-	return render(request, "the_portfolio/index.html")
+class AddProject(CreateView):
+	template_name = 'the_portfolio/add_project.html'
+	model = Project
+	fields = ['project_name' , 'project_description']
+	success_url = '/'
