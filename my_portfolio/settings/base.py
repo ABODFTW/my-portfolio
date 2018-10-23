1a1ee29305936f057b9ae3ca07c2aaf9095b4909
ABOD <abod80000@gmail.com>
2018-10-23 03:53:26 +0200

    sprated allowed hosts & fixed base_dir in base.py"

--- my_portfolio/settings/base.py
+++ my_portfolio/settings/base.py
@@ -2,22 +2,13 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-ALLOWED_HOSTS = [
-'0.0.0.0',
-'127.0.0.1',
-'*'
-]
 
 
 # Application definition
--- my_portfolio/settings/production.py
+++ my_portfolio/settings/production.py
@@ -2,3 +2,9 @@ from .base import *
 
 
 DEBUG = False
+
+ALLOWED_HOSTS = [
+'0.0.0.0',
+'127.0.0.1',
+'*'
+]
