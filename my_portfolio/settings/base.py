94bb93b446ac663ee67829f06c7724504eb9e43a
ABOD <abod80000@gmail.com>
2018-12-22 16:14:55 +0200

    Updated the personal photo and add a bg photo, added an icon and smooth scrolling.

--- my_portfolio/settings/base.py
+++ my_portfolio/settings/base.py
@@ -3,6 +3,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -101,8 +102,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 django_heroku.settings(locals())
